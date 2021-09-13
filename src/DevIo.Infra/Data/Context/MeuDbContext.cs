070bf4d8198ac3fb64bff66f6dee0e94a5e0de5e
Karollainny Teles <karollainnyteles@hotmail.com>
2021-09-13 11:34:40 -0300

    Configurando o DbContext

---
+++ src/DevIo.Infra/Data/Context/MeuDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevIo.Business.Models.Fornecedores;
+using DevIo.Business.Models.Produtos;
+
+namespace DevIo.Infra.Data.Context
+{
+    class MeuDbContext : DbContext
+    {
+        public MeuDbContext() : base("DefaultConnection")
+        {}
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
