0aebbd4ead763af520d27f2fd51b0d9e0764dc5a
Karollainny Teles <karollainnyteles@hotmail.com>
2021-09-15 11:12:10 -0300

    Implemetando Repositórios especializados

---
+++ src/DevIo.Infra/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,18 @@
+﻿using DevIo.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Infra.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await ObterPorId(fornecedorId);
+        }
+    }
+
+}
---
+++ src/DevIo.Infra/Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,27 @@
+﻿using DevIo.Business.Models.Fornecedores;
+using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Infra.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(f => f.Endereco)
+                .FirstOrDefaultAsync(f => f.Id == id);// faz um join de fornecedores e endereco
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(f => f.Endereco)
+                .Include(f => f.Produtos)
+                .FirstOrDefaultAsync(f => f.Id == id);// faz um join de fornecedores, produtos e endereco
+        }
+}
